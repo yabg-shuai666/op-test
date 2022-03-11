c06d6e087f57d71c0a1e341f3ed0b3dfbd451816
yangshuai <326605441@qq.com>
2022-03-11 15:13:44 +0800

    代码优化,top3frequency op

--- applications/feature-zero/python/test/generate_cases.py
+++ applications/feature-zero/python/test/generate_cases.py
@@ -24,7 +24,7 @@ def case():
     curPath = os.path.dirname(os.path.realpath(__file__))
     print(curPath)
     # 获取yaml文件路径
-    yamlPath = os.path.join(curPath, "op_convert_case/data_multi_sum.yaml")
+    yamlPath = os.path.join(curPath, "op_convert_case/data_multi_top3frequency.yaml")
  
     # open方法打开直接读出来
     f = open(yamlPath, 'r', encoding='utf-8')
